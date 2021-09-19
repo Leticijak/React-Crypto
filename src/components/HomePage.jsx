2fa4c8fb3b5d53507ef7d37831ee160b6a27d7fb
Leticijak <mnogomijeheavy@gmail.com>
2021-09-19 16:57:31 +0200

    works

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { Title } = Typography;
+
+  const { data, isFetching } = useGetCryptosQuery();
+
+  console.log(data);
+  return (
+    <>
+      <Title level={1} className="heading">
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total crypto currencies" value="5" />{" "}
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total exchanges" value="5" />{" "}
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total market cap" value="5" />{" "}
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total 24hour volume" value="5" />{" "}
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total Markets" value="5" />{" "}
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default HomePage;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "antd/dist/antd.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./app/store";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
   document.getElementById("root")
 );
