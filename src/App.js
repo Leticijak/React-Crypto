32e803839162c998dd3a983befbf90a1475bc8b5
Leticijak <mnogomijeheavy@gmail.com>
2021-09-19 15:35:13 +0200

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
-
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import { Navbar } from "./components";
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
     </div>
   );
 }
