f369c3c6baafcd2c1734089bc310947448be8473
Leticijak <mnogomijeheavy@gmail.com>
2021-09-19 17:14:59 +0200

    ok

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import millify from "millify";
+
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
+import millify from "millify";
 
 const HomePage = () => {
   const { Title } = Typography;
 
   const { data, isFetching } = useGetCryptosQuery();
 
+  // global stats
+  const globalStats = data?.data?.stats;
+
   console.log(data);
+  if (isFetching) return "Loading ...";
   return (
     <>
       <Title level={1} className="heading">
@@ -18,24 +23,34 @@ const HomePage = () => {
       </Title>
       <Row>
         <Col span={12}>
-          {" "}
-          <Statistic title="Total crypto currencies" value="5" />{" "}
+          <Statistic
+            title="Total crypto currencies"
+            value={globalStats.total}
+          />
         </Col>
         <Col span={12}>
-          {" "}
-          <Statistic title="Total exchanges" value="5" />{" "}
+          <Statistic
+            title="Total exchanges"
+            value={globalStats.totalExchanges}
+          />
         </Col>
         <Col span={12}>
-          {" "}
-          <Statistic title="Total market cap" value="5" />{" "}
+          <Statistic
+            title="Total market cap"
+            value={millify(globalStats.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          {" "}
-          <Statistic title="Total 24hour volume" value="5" />{" "}
+          <Statistic
+            title="Total 24hour volume"
+            value={millify(globalStats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          {" "}
-          <Statistic title="Total Markets" value="5" />{" "}
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />
         </Col>
       </Row>
     </>
